47afd9a221b0916d8b83d0c885f3608bf62dec50
Nik Luparev <nikita.luparev@gmail.com>
2023-09-23 13:12:54 +0500

    add styles to all components

--- components/DetailsPage.js
+++ components/DetailsPage.js
@@ -1,6 +1,28 @@
 export default class DetailsPage extends HTMLElement {
   constructor() {
     super();
+
+    this.root = this.attachShadow({ mode: 'open' });
+
+    this.styles = document.createElement('style');
+
+    this.root.appendChild(this.styles);
+
+    this.loadCSS();
+  }
+
+  async loadCSS() {
+    const request = await fetch('/components/DetailsPage.css');
+    const css = await request.text();
+
+    this.styles.textContent = css;
+  }
+
+  connectedCallback() {
+    const template = document.getElementById('product-item-template');
+    const content = template.content.cloneNode(true);
+
+    this.root.appendChild(content);
   }
 }
 
