a3deb9c2176d016861437c9ad31281368705fa05
Nik Luparev <nikita.luparev@gmail.com>
2023-09-25 12:25:41 +0500

    order page

--- components/OrderPage.js
+++ components/OrderPage.js
@@ -3,26 +3,58 @@ export default class OrderPage extends HTMLElement {
     super();
 
     this.root = this.attachShadow({ mode: 'open' });
+    const styles = document.createElement('style');
+    this.root.appendChild(styles);
+    const section = document.createElement('section');
+    this.root.appendChild(section);
 
-    this.styles = document.createElement('style');
-
-    this.root.appendChild(this.styles);
-
-    this.loadCSS();
-  }
-
-  async loadCSS() {
-    const request = await fetch('/components/OrderPage.css');
-    const css = await request.text();
-
-    this.styles.textContent = css;
+    async function loadCSS() {
+      const request = await fetch('/components/OrderPage.css');
+      styles.textContent = await request.text();
+    }
+    loadCSS();
   }
 
   connectedCallback() {
-    const template = document.getElementById('order-form-template');
-    const content = template.content.cloneNode(true);
+    window.addEventListener('appcartchange', () => {
+      this.render();
+    });
+    this.render();
+  }
 
-    this.root.appendChild(content);
+  render() {
+    let section = this.root.querySelector('section');
+    if (app.store.cart.length == 0) {
+      section.innerHTML = `
+          <p class="empty">Your order is empty</p>
+      `;
+    } else {
+      let html = `
+          <h2>Your Order</h2>
+          <ul>
+          </ul>
+      `;
+      section.innerHTML = html;
+
+      const template = document.getElementById('order-form-template');
+      const content = template.content.cloneNode(true);
+      section.appendChild(content);
+
+      let total = 0;
+      for (let prodInCart of app.store.cart) {
+        const item = document.createElement('cart-item');
+        item.dataset.item = JSON.stringify(prodInCart);
+        this.root.querySelector('ul').appendChild(item);
+
+        total += prodInCart.quantity * prodInCart.product.price;
+      }
+      this.root.querySelector('ul').innerHTML += `
+            <li>
+                <p class='total'>Total</p>
+                <p class='price-total'>$${total.toFixed(2)}</p>
+            </li>                
+        `;
+    }
   }
 }
 
